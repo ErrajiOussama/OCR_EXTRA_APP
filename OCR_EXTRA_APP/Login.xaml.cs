7642dae26d8d7047f2e75a9aa44236c0a45d9107
OUSSAMA ERRAJI <ERRAJI_OUSSAMA@emsi-edu.ma>
2022-07-12 11:29:34 +0100

    Add project files.

---
+++ OCR_EXTRA_APP/Login.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OCR_EXTRA_APP
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        string _connexionString = "";
+        public Login()
+        {
+            InitializeComponent();
+            try
+            {
+                // Chargement de la chaine de connexion
+                var builder = new ConfigurationBuilder().AddJsonFile($"./config.json").Build();
+                _connexionString = builder["ConnexionString"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Login1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(_connexionString))
+                {
+                    conn.Open();
+                    var sqlLogin = $"select * from Compte where login=@login AND password=@password;";
+                    using (NpgsqlCommand npgsqlCommand = new NpgsqlCommand(sqlLogin, conn))
+                    {
+                        npgsqlCommand.Parameters.Add(new NpgsqlParameter("@login", UserName.Text.Trim()));
+                        npgsqlCommand.Parameters.Add(new NpgsqlParameter("@password", Password.Password.Trim()));
+
+                        var reader = npgsqlCommand.ExecuteReader();
+
+                        if (reader.HasRows)
+                        {
+                            Menu menu = new Menu();
+                            menu.Show();
+                            this.Close();
+                        }
+                        else
+                            MessageBox.Show("erreur login ou mdp");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Trace.WriteLine(ex);
+            }
+        }
+    }
+    }
+
---
+++ OCR_EXTRA_APP/Menu.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace OCR_EXTRA_APP
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        DispatcherTimer timer;
+
+        double panelWidth;
+        bool hidden;
+        Etat_Civil etat_civil;
+        Correction correction;
+        OSRisation ocrisation;
+        Parametre paramatrie;
+        public Menu()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
+
+            
+            InitializeComponent();
+        }
+            private void initColor()
+            {
+                menuEtat_civil.Foreground = Brushes.White;
+                menuEtat_civil.Background = Brushes.Orange;
+                menuCorrection.Foreground = Brushes.White;
+                menuCorrection.Background = Brushes.Orange;
+            }
+
+            private void menuEtat_civil_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+            {
+                if (etat_civil != null)
+                {
+                    MainFrame.Navigate(etat_civil);
+                }
+                else
+                {
+                    etat_civil = new Etat_Civil();
+                    MainFrame.Navigate(etat_civil);
+                }
+            }
+
+            private void menuCorrection_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+            {
+                if (correction != null)
+                {
+                    MainFrame.Navigate(correction);
+                }
+                else
+                {
+                    correction = new Correction();
+                    MainFrame.Navigate(correction);
+                }
+            }
+            private void menuOSResation_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+            {
+                if (ocrisation != null)
+                {
+                    MainFrame.Navigate(ocrisation);
+                }
+                else
+                {
+                    ocrisation = new OSRisation();
+                    MainFrame.Navigate(ocrisation);
+                }
+            }
+            private void menuPrametrie_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+            {
+                if (paramatrie != null)
+                {
+                    MainFrame.Navigate(paramatrie);
+                }
+                else
+                {
+                    paramatrie = new Parametre();
+                    MainFrame.Navigate(paramatrie);
+                }
+            }
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                if (hidden)
+                {
+                    sidePanel.Width += 1;
+                    if (sidePanel.Width >= panelWidth)
+                    {
+                        timer.Stop();
+                        hidden = false;
+                    }
+                }
+                else
+                {
+                    sidePanel.Width -= 1;
+                    if (sidePanel.Width <= 35)
+                    {
+                        timer.Stop();
+                        hidden = true;
+                    }
+                }
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                timer.Start();
+            }
+
+            private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    DragMove();
+                }
+            }
+
+            private void menu_btn_Click(object sender, RoutedEventArgs e)
+            {
+
+            }
+        }
+    }
