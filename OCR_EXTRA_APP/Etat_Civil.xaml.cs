90a108bd6ab901a9674f916ce752157c124d3b1b
OUSSAMA ERRAJI <ERRAJI_OUSSAMA@emsi-edu.ma>
2022-07-12 14:53:04 +0100

    commit2

--- OCR_EXTRA_APP/Etat_Civil.xaml.cs
+++ OCR_EXTRA_APP/Etat_Civil.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OCR_EXTRA_APP
 {
@@ -18,9 +12,31 @@ namespace OCR_EXTRA_APP
     /// </summary>
     public partial class Etat_Civil : Page
     {
+        string _connexionString = "";
         public Etat_Civil()
         {
             InitializeComponent();
+
+            ContextMenu menuListLot = this.FindResource("cmListLot") as ContextMenu;
+            EmployeesList.ContextMenu = menuListLot;
+
+            try
+            {
+                // Chargement de la chaine de connexion
+                var builder = new ConfigurationBuilder().AddJsonFile($"./config.json").Build();
+                _connexionString = builder["ConnexionString"];
+                var sqlcomptes = $"select id,login,password,role from Compte;";
+                using (NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(sqlcomptes, _connexionString))
+                {
+                    DataTable dataTable = new DataTable();
+                    npgsqlDataAdapter.Fill(dataTable);
+                    EmployeesList.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
