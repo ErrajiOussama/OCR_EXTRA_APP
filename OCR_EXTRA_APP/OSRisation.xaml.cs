3e304ea812068c88dfc2957578ff96fb8eec47a6
OUSSAMA ERRAJI <ERRAJI_OUSSAMA@emsi-edu.ma>
2022-07-20 17:17:06 +0100

    commit6

--- OCR_EXTRA_APP/OSRisation.xaml.cs
+++ OCR_EXTRA_APP/OSRisation.xaml.cs
@@ -79,6 +79,76 @@ namespace OCR_EXTRA_APP
                     MessageBox.Show(ex.Message);
                 }
             }
+
         }
+        private void search_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(search.Text))
+                    {
+                        #region Methode Search
+                        var type_value = "";
+                        switch (combo.SelectedIndex)
+                        {
+                            case 0:
+                                type_value = "id_bureau";
+                                break;
+                            case 1:
+                                type_value = "id_commune";
+                                break;
+                            case 2:
+                                type_value = "id_lot";
+                                break;
+                            default:
+                                type_value = "id_lot";
+                                break;
+                        }
+                        DataTable searchTable = _dataTableListLot.Copy();
+                        searchTable.DefaultView.RowFilter = $"convert({type_value}, System.String) like '%{search.Text.Trim()}%'";
+                        LotsList.ItemsSource = searchTable.DefaultView;
+                        #endregion
+
+                        #region Methode search database
+                        //var type_value = "";
+                        //switch(combo.SelectedIndex)
+                        //{
+                        //    case 0:
+                        //        type_value = "l.id_bureau";
+                        //        break;
+                        //    case 1:
+                        //        type_value = "l.id_commune";
+                        //        break;
+                        //    case 2:
+                        //        type_value = "l.id_lot";
+                        //        break;
+                        //    default :
+                        //        type_value = "l.id_lot";
+                        //        break;
+                        //}
+
+                        //var sql = (new StreamReader(@"SQL/search_from_type.sql")).ReadToEnd().Replace("@type",type_value).Replace("@valeur",$"'%{search.Text.Trim()}%'");
+                        //using (var dataAdapter = new NpgsqlDataAdapter(sql, _connexionString))
+                        //{                    
+                        //    DataTable dataTable = new DataTable();
+                        //    dataAdapter.Fill(dataTable);
+                        //    EmployeesList.ItemsSource = dataTable.DefaultView;
+                        //}
+                        #endregion
+                    }
+                    else
+                    {
+                        LotsList.ItemsSource = _dataTableListLot.DefaultView;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
     }
 }
